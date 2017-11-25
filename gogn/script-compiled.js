e9e8330f391cc37609e769860d79b9a2a8c49f2d
hyn1 <32751827+hyn1@users.noreply.github.com>
2017-11-25 01:10:18 +0000

    backup

--- gogn/script-compiled.js
+++ gogn/script-compiled.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 "use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -43,6 +41,5 @@ var VideoLoader = function () {
 document.addEventListener('DOMContentLoaded', function () {
   var videoLoader = new VideoLoader(document.querySelector('main'));
 });
->>>>>>> a2c25737a7d3ea031d6db35439e1ab2c8c564377
 
 //# sourceMappingURL=script-compiled.js.map
